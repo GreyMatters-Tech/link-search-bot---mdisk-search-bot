b8ddf204aba150803ebc6208ebe941360b9f500e
GreyMatter's <greymattersyt@gmail.com>
2022-11-30 17:28:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,8 @@ async def button(bot, cmd: CallbackQuery):
 		)
 self.username = usr_bot_me.username
 #web-response
-app = web.AppRunner(await web_server())
+app = web.AppRunner()
+await web_server()
 await app.setup()
 bind_address = "0.0.0.0"
 await web.TCPSite(app, bind_address, PORT).start()
