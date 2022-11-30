4ff769f5ed1334919100736b9246764d9fd08823
GreyMatter's <greymattersyt@gmail.com>
2022-11-30 15:11:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
 cd /Mdisk-Search-Bot
 pip3 install -r requirements.txt
-echo "Starting Bot....ANURAG"
+echo "Starting Bot....GreyMatter_Bots"
 python3 main.py
