4fcaa0b644e4fcf70634284ab8212ee8201391c3
GreyMatter's <greymattersyt@gmail.com>
2022-11-30 15:38:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,6 +5,7 @@ class Config(object):
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    PORT = os.environ.get("PORT", "8080")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "LinkSearchBot")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
     CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
