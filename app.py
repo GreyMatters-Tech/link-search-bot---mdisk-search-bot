ab9c6b6b6f5b9071f42bb0b1ba1ba031475214b3
GreyMatter's <greymattersyt@gmail.com>
2022-12-18 19:57:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'GreyMatters'
-
-
-if __name__ == "__main__":
-    app.run()
+   return 'Hello, World!'
 
 os.system("git clone -b test https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot okk && cd okk && pip3 install -U -r requirements.txt && gunicorn app:app && python3 main.py &")
