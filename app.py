9e69021c5ffd8875e4a29b769d6eecf1ecfd2c0b
GreyMatter's <greymattersyt@gmail.com>
2022-12-18 20:09:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
+#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
 os.system("git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot okk && cd okk && pip3 install -U -r requirements.txt && gunicorn app:app && python3 main.py &")
