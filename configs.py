bea0c5d93f6e69d63ae39d95a50aefd08fb26f92
GreyMatter's <greymattersyt@gmail.com>
2022-12-18 11:06:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,16 +2,16 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 10683462))
+    API_HASH = os.environ.get("API_HASH", "8ab812d6e6849bd6352dcb731e44c31e")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5680348811:AAFTg0TmzNx6dBPG5A2TQ3meJq3_npGVs2A")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "LinkSearchBot")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQBbv3ZEDjihY4mXoFdZTyr9OqFeTqEaDqBYqby8x9HVYpSx0y8U18FiEbDIK73PnjEJdNEzy_cueymotgCEWBSdVmd8iIv7tShqQyve-wREJIEub9nUQwk9w8izL_nUC-Okaa_ljGzYV7hieMUFbCA5M6E_Ml4HTkvAo2pqsOolFBzzPbgg1booF24oAtUR0kvNNuWai6iUGOoGtp3xpT-IUC8z--JbH7M5xHpPCQ716Xf6Em85kTYkg6Im9OtjQOCoLZhua8U8cw9npu_2iIUq43Wb7oBr6ieiAvX7KahUZwDoUvnANhPtyFTaE-E2g_gnif60W_1l4yRhjso9IHN7AAAAAVLdr70A")
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001838491668"))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "greymatters_test_bot")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", "5685227453"))
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Test:1234@cluster0.2bzsp0q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "udhdin")
     ABOUT_BOT_TEXT = """<b>This is Link Search Bot.
     
     
